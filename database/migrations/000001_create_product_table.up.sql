1a035beec377503ea61148d3f3b89e209263e123
themethaithian <thaithian1999@gmail.com>
2024-08-14 01:03:30 +0700

    feat: database migration for user and product

--- database/migrations/000001_create_product_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "tbl_product" (
-    "id" varchar NOT NULL,
-    "name" varchar NOT NULL,
-    "description" varchar,
-    "price" numeric NOT NULL DEFAULT 0.00,
-    "quantity" int4 NOT NULL DEFAULT 0,
-    PRIMARY KEY ("id")
-);
